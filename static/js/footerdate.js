a03ced16f13f2fda8a99620e4f4c7571937acaea
jonathanw <jonwhewaycode@outlook.com>
2022-10-29 16:23:28 +0100

    sorting bugs

--- static/js/footerdate.js
+++ static/js/footerdate.js
@@ -1,9 +1,6 @@
 "use strict";
 
-let brewkettleyear = document.getElementById('brewkettleyear');
 let footerdate = document.getElementById("footeryear");
 let date = new Date().getFullYear();
 
-brewkettleyear.innerHTML = `${date}`
 footerdate.innerHTML = `2016-${date}`;
-
