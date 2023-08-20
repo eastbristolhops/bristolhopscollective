c6b9bc12910151208421659428d506bb695e9f98
jonathanw <jonwhewaycode@outlook.com>
2023-08-20 18:28:27 +0100

    added new links and sorted some bugs to the navbar

--- static/newsEvents/news_event_info.js
+++ static/newsEvents/news_event_info.js
@@ -4,13 +4,20 @@ let news =[
     "<b style='color: red'>*** BIG NEWS ***</b>  We have changed our name! Formerly known as East Bristol Hops, going forward, our network of patchwork \
     farmers will be known as <b>Bristol Hops Collective</b>. This will better reflect the reach of our growers and make \
     us more inclusive. We also hope this will clear up any confusion prospective new members may have over who may join us. \
-    For years Easton Promise has comprised of hops grown all over Bristol and we decided it was time our name was more representative of all our growers." 
+    For years Easton Promise has comprised of hops grown all over Bristol and we decided it was time our name was more representative of all our growers.",
+    "<b style='color: red'>*** EVEN BIGGER NEWS ***</b> Earlier in the year we said a fond farewell to Dawkins Ales, as they sadly had to close their doors for \
+    the last time. However, we are please to announce <a href='https://www.newbristolbrewery.co.uk' target='_blank'>New Bristol Brewery</a> as the official \
+     brewer of the 2023 Bristol Hops Collective brew.  <img class='pumpimage' src='../static/media/newbristol.png' alt='pumpclip image'>"
 ]
 let events =[
+     "The Legendary Cropathon/weigh-in @ New Bristol Brewery Tap Room, 20A Wilson St, St Paul's, Bristol BS2 9HH",
+     'Launch party same location as above dates are',
      'We now have a page dedicated page for news ',
      'For updates about Bristol Hops Collective'
 ]
 let events_dates_times = [
+     'Wednesday 20th September',
+     'TBC',
      '<a href="pages/news.html">check it out.</a>',
      '<a href="pages/subscribe.html">sign up to the mailing list.</a>'
 ]
